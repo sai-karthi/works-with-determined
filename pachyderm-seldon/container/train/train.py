e037455aae8a8a694b17bad3a05dc88148050f37
JordanNanos <jordannanos@hotmail.com>
2022-08-17 15:37:32 -0400

    asdf

--- pachyderm-seldon/container/train/train.py
+++ pachyderm-seldon/container/train/train.py
@@ -122,7 +122,8 @@ def create_client():
 
 def execute_experiment(client, configfile, code_path, parent_id):
     print("Parent ID = " + str(parent_id))
-    print(parent_id.uuid)
+    print(parent_id.trial_id)
+    print(parent_id.Checkpoint.trial_id)
     try:
         if parent_id is None:
             exp = client.create_experiment(configfile, code_path)
