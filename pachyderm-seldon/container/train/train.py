797c40429110ecaa7d4ce18b80a7bf461a7f02c4
JordanNanos <jordannanos@hotmail.com>
2022-08-17 14:48:43 -0400

    17

--- pachyderm-seldon/container/train/train.py
+++ pachyderm-seldon/container/train/train.py
@@ -122,14 +122,12 @@ def create_client():
 
 def execute_experiment(client, configfile, code_path, parent_id):
     print("Parent ID = " + str(parent_id))
-    print(str(parent_id.uuid))
     print(parent_id.uuid)
-    print(parent_id.checkpoint.uuid)
     try:
         if parent_id is None:
             exp = client.create_experiment(configfile, code_path)
         else:
-            exp = client.continue_experiment(configfile, parent_id, str(parent_id.uuid))
+            exp = client.continue_experiment(configfile, parent_id, parent_id.uuid)
 
         print(f"Created experiment with id='{exp.id}' (parent_id='{parent_id}'). Waiting for its completion...")
 
