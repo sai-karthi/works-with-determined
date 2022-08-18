62ae46ba127d0080afe6fe51677228af514416ed
JordanNanos <jordannanos@hotmail.com>
2022-08-18 12:57:22 -0400

    update

--- pachyderm-seldon/use-case/brain-mri/experiment/data.py
+++ pachyderm-seldon/use-case/brain-mri/experiment/data.py
@@ -1,37 +1,79 @@
 import os
-import shutil
 
-import python_pachyderm
-from python_pachyderm.proto.v2.pfs.pfs_pb2 import FileType
-import torch
+import numpy as np
+import pandas as pd
+
 from PIL import Image
+import cv2
 
-from skimage import io
-from torch.utils.data import Dataset
+import torch
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
 
-# ======================================================================================================================
+from sklearn.model_selection import train_test_split
 
-class CatDogDataset(Dataset):
-    def __init__(self, files, transform=None):
-        self.files     = files
+
+class MRI_Dataset(Dataset):
+    def __init__(self, path_df, data_dir, transform=None):
+        self.path_df = path_df
         self.transform = transform
-
+        self.data_dir = data_dir
+        
     def __len__(self):
-        return len(self.files)
-
+        return self.path_df.shape[0]
+    
     def __getitem__(self, idx):
-        if torch.is_tensor(idx):
-            idx = idx.tolist()
-
-        img_name = self.files[idx]
-        image = io.imread(img_name)
-        image = Image.fromarray(image)
+        
+        base_path = os.path.join(self.data_dir, self.path_df.iloc[idx]['directory'])
+        img_path = os.path.join(base_path, self.path_df.iloc[idx]['images'])
+        mask_path = os.path.join(base_path, self.path_df.iloc[idx]['masks'])
+        
+        image = Image.open(img_path)
+        mask = Image.open(mask_path)
+        
+        sample = (image, mask)
+        
         if self.transform:
-            image = self.transform(image)
-        label = 0 if img_name.startswith('dog') else 1
-        sample = (image, label)
-        # print(f"Loaded image: index='{idx}', name='{img_name}'")
+            sample = self.transform(sample)
+            
         return sample
+    
+class PairedToTensor():
+    def __call__(self, sample):
+        img, mask = sample
+        img = np.array(img)
+        mask = np.expand_dims(mask, -1)
+        img = np.moveaxis(img, -1, 0)
+        mask = np.moveaxis(mask, -1, 0)
+        img, mask = torch.FloatTensor(img), torch.FloatTensor(mask)
+        img = img/255
+        mask = mask/255
+        return img, mask
+    
+def get_train_val_datasets(data_dir, seed, validation_ratio=0.2):
+    
+    dirs, images, masks = [], [], []
+
+    for root, folders, files in  os.walk(data_dir):
+        for file in files:
+            if 'mask' in file:
+                dirs.append(root.replace(data_dir, ''))
+                masks.append(file)
+                images.append(file.replace("_mask", ""))
+                
+    PathDF = pd.DataFrame({'directory': dirs,
+                          'images': images,
+                          'masks': masks})
+
+    train_df, valid_df = train_test_split(PathDF, random_state=seed,
+                                     test_size = validation_ratio)
+    
+    train_data = MRI_Dataset(train_df, data_dir, transform=PairedToTensor())
+    valid_data = MRI_Dataset(valid_df, data_dir, transform=PairedToTensor())
+    
+    return train_data, valid_data
+
+
 
 # ======================================================================================================================
 
--- pachyderm-seldon/use-case/brain-mri/experiment/model_def.py
+++ pachyderm-seldon/use-case/brain-mri/experiment/model_def.py
@@ -16,7 +16,11 @@ class MRIUnetTrial(PyTorchTrial):
         self.context = context
         self.config = self.context.get_data_config()
         
-        self.train_dataset, self.val_dataset = data.get_train_val_datasets(self.config["data_dir"],
+        self.download_directory = f"/tmp/data-rank{self.context.distributed.get_rank()}"
+
+        self.download_data()
+
+        self.train_dataset, self.val_dataset = data.get_train_val_datasets(self.download_directory,
                                                                            self.context.get_hparam("split_seed"),
                                                                            self.context.get_hparam("validation_ratio"))
         
@@ -68,3 +72,119 @@ class MRIUnetTrial(PyTorchTrial):
 
     def build_validation_data_loader(self):
         return DataLoader(self.val_dataset, batch_size=self.context.get_per_slot_batch_size())
+
+
+
+
+    # -------------------------------------------------------------------------
+    
+    ### MODEL_DEF from image-classification below
+    
+    # -------------------------------------------------------------------------
+
+    def train_batch(self, batch: TorchData, epoch_idx: int, batch_idx: int) -> Union[torch.Tensor, Dict[str, Any]]:
+        batch = cast(Tuple[torch.Tensor, torch.Tensor], batch)
+        data, labels = batch
+
+        output = self.model(data)
+        loss = torch.nn.functional.cross_entropy(output, labels)
+
+        self.context.backward(loss)
+        self.context.step_optimizer(self.optimizer)
+
+        return {"loss": loss}
+
+    # -------------------------------------------------------------------------
+
+    def evaluate_batch(self, batch: TorchData, batch_idx: int) -> Dict[str, Any]:
+        """
+        Calculate validation metrics for a batch and return them as a dictionary.
+        This method is not necessary if the user overwrites evaluate_full_dataset().
+        """
+        batch = cast(Tuple[torch.Tensor, torch.Tensor], batch)
+        data, labels = batch
+        output = self.model(data)
+
+        pred = output.argmax(dim=1, keepdim=True)
+        accuracy = pred.eq(labels.view_as(pred)).sum().item() / len(data)
+        return {"accuracy": accuracy}
+
+    # -------------------------------------------------------------------------
+
+    def build_training_data_loader(self) -> DataLoader:
+        return DataLoader(self.train_ds, batch_size=self.context.get_per_slot_batch_size())
+
+    # -------------------------------------------------------------------------
+
+    def build_validation_data_loader(self) -> DataLoader:
+        return DataLoader(self.val_ds, batch_size=self.context.get_per_slot_batch_size())
+
+    # -------------------------------------------------------------------------
+
+    def download_data(self):
+        data_config = self.context.get_data_config()
+        data_dir = os.path.join(self.download_directory, 'data')
+
+        files = download_pach_repo(
+            data_config['pachyderm']['host'],
+            data_config['pachyderm']['port'],
+            data_config["pachyderm"]["repo"],
+            data_config["pachyderm"]["branch"],
+            data_dir,
+            data_config["pachyderm"]["token"]
+        )
+        print(f'Data dir set to : {data_dir}')
+
+        return [des for src, des in files ]
+
+    # -------------------------------------------------------------------------
+
+    def create_datasets(self, files):
+        print(f"Creating datasets from {len(files)} input files")
+        train_size = round(0.81 * len(files))
+        val_size   = len(files) - train_size
+        train_ds, val_ds = torch.utils.data.random_split(files, [train_size, val_size])
+
+        self.train_ds = CatDogDataset(train_ds, transform=self.get_train_transforms())
+        self.val_ds   = CatDogDataset(val_ds,   transform=self.get_test_transforms())
+        print(f"Datasets created: train_size={train_size}, val_size={val_size}")
+
+    # -------------------------------------------------------------------------
+
+    def get_train_transforms(self):
+        return transforms.Compose([
+            transforms.Resize(240),
+            transforms.RandomCrop(224),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+        ])
+
+    # -------------------------------------------------------------------------
+
+    def get_test_transforms(self):
+        return transforms.Compose([
+            transforms.Resize(240),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+        ])
+
+    # -------------------------------------------------------------------------
+
+    def predict(self, X: np.ndarray, names, meta) -> Union[np.ndarray, List, str, bytes, Dict]:
+
+        image = Image.fromarray(X.astype(np.uint8))
+        logging.info(f"Image size : {image.size}")
+
+        image = self.get_test_transforms()(image)
+        image = image.unsqueeze(0)
+
+        with torch.no_grad():
+            output = self.model(image)[0]
+            pred = np.argmax(output)
+            logging.info(f"Prediction is : {pred}")
+
+        return [self.labels[pred]]
+
+# =============================================================================
