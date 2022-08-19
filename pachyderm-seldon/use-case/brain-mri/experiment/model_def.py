3800d3b4fb7922143a8924c1178248e715169ab5
JordanNanos <jordannanos@hotmail.com>
2022-08-19 10:05:31 -0400

    Update model_def.py

--- pachyderm-seldon/use-case/brain-mri/experiment/model_def.py
+++ pachyderm-seldon/use-case/brain-mri/experiment/model_def.py
@@ -30,7 +30,7 @@ class MRIUnetTrial(PyTorchTrial):
         print("HEYOOOOO")
 
         full_dir = "/"
-        os.path.join(full_dir, download_dir.strip("/"), data_dir.strip("/"))
+        full_dir = os.path.join(full_dir, download_dir.strip("/"), data_dir.strip("/"))
 
         print("full_dir = " + full_dir)
 
