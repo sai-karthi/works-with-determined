56f5613e741862cef314963f20db8b49e1b293bc
JordanNanos <jordannanos@hotmail.com>
2022-09-20 15:20:31 -0700

    update from Cyrill!

--- pachyderm-seldon/use-case/brain-mri/container/serve/ModelServer copy.py
+++ pachyderm-seldon/use-case/brain-mri/container/serve/ModelServer copy.py
@@ -1,19 +1,18 @@
-import logging
 import os
 import time
-from typing import Dict, List, Optional, Union
+from typing import Dict, Union, List, Optional
 
-import numpy as np
-import torch
 from determined.common.experimental import ModelVersion
 from determined.experimental import Determined
 from determined.pytorch import load_trial_from_checkpoint_path
+import numpy as np
+import logging
+import torch
 
 logging.basicConfig()
 logging.getLogger().setLevel(logging.INFO)
 
 # =============================================================================
-
 
 class ModelServer(object):
     """
@@ -24,23 +23,22 @@ class ModelServer(object):
         logging.info(f"Loading model version '{model_name}/{model_version}' from master at '{det_master}'")
 
         # Credentials to download the checkpoint from the bucket
-        os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "config/service-account.json"
+        os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'config/service-account.json'
 
-        if os.environ["HOME"] == "/":
-            os.environ["HOME"] = "/app"
+        if os.environ['HOME'] == '/':
+            os.environ['HOME'] = '/app'
 
-        os.environ["SERVING_MODE"] = "true"
+        os.environ['SERVING_MODE'] = 'true'
 
         start = time.time()
         client = Determined(master=det_master, user=user, password=password)
         version = self.get_version(client, model_name, model_version)
         checkpoint = version.checkpoint
         checkpoint_dir = checkpoint.download()
-        self.trial = load_trial_from_checkpoint_path(checkpoint_dir, map_location=torch.device("cpu"))
-        self.model = self.trial.model
+        self.model = load_trial_from_checkpoint_path(checkpoint_dir, map_location=torch.device('cpu'))
         end = time.time()
         delta = end - start
-        logging.info(f"Checkpoint loaded in {delta} seconds")
+        logging.info(f'Checkpoint loaded in {delta} seconds')
 
     # -------------------------------------------------------------------------
 
@@ -54,24 +52,17 @@ class ModelServer(object):
 
     # -------------------------------------------------------------------------
 
-    def predict(self, X: Union[np.ndarray, List, str, bytes, Dict]) -> Union[np.ndarray, List, str, bytes, Dict]:
-
+    def predict(self, X: Union[np.ndarray, List, str, bytes, Dict], names: Optional[List[str]],
+                meta: Optional[Dict] = None) -> Union[np.ndarray, List, str, bytes, Dict]:
         logging.info(f"Received request : \n{X}")
 
-        input_img = X.unsqueeze(0)
-
         try:
-            with torch.no_grad():
-                prediction = self.model(input_img)
-
-            prediction = torch.round(prediction[0])
+            prediction = self.model.predict(X, names, meta)
             logging.info(f"Prediction : {prediction}")
 
             return prediction
-
         except Exception as e:
             logging.warning(f"Raised error : {e}")
             return "???"
-
 
 # =============================================================================
